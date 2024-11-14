0b2885813cda3498f46bd3b7625388ab37712f83
Vikram <htchonda4@gmail.com>
2024-11-14 14:14:34 +0530

    snapkart project start

---
+++ android/app/src/main/kotlin/com/example/snapkart_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.snapkart_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApplication());
+}
+
+class MyApplication extends StatelessWidget {
+  const MyApplication({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(),
+    );
+  }
+}
