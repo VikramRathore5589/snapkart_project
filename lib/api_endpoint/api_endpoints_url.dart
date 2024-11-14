8ce8f67ecf6bf5bc413c1bbaac29d560ece79345
Vikram <htchonda4@gmail.com>
2024-11-14 14:34:48 +0530

    create ApiUrl class

---
+++ lib/api_endpoint/api_endpoints_url.dart
@@ -0,0 +1,5 @@
+class ApiUrl{
+  static const baseUrl='https://e-commerce-server-zc33.onrender.com';
+  static const signUpUrl='$baseUrl/api/users/register';
+  static const loginUrl='$baseUrl/api/users/login';
+}
