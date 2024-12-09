226d86685e536c6b6561095a308c72fa640d7e44
Vikram <htchonda4@gmail.com>
2024-12-09 08:49:02 +0530

    change in profile screen

--- lib/profile/screen/profile_screen.dart
+++ lib/profile/screen/profile_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:snapkart_project/auth/provider/auth_provider.dart';
+import 'package:snapkart_project/auth/screen/log_in_screen.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -7,7 +10,25 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Cart Screen')),
+        title: const Center(child: Text('Profile Screen')),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: IconButton(
+                  onPressed: () {
+                    AuthProvider authProvider =
+                        Provider.of<AuthProvider>(context, listen: false);
+                    authProvider.logOut();
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LogInScreen(),
+                        ));
+                  },
+                  icon: const Icon(Icons.logout)))
+        ],
       ),
     );
   }
